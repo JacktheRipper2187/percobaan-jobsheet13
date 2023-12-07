b32a059c2ce390b31998347fb7f6750ca95f8019
JackTheRipper2187 <ulilf442@gmail.com>
2023-12-07 19:36:59 +0700

    kode percobaan 4 sebelum di modifikasi

---
+++ UcapanTerimaKasih_22.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UcapanTerimaKasih_22 {
+
+    public static String PenerimaUcapan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan: ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+    public static void UcapanTerimakasih(){
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+" for being the best teacher in the world. \n"+
+        "You inspires in me a love for learning and made me feel like I could ask you anything. ");
+    }
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+        
+    }
+}
