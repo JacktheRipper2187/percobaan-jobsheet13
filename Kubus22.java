83a9ca2d8490e84b08f80356892f10620f53bc1d
JackTheRipper2187 <ulilf442@gmail.com>
2023-12-11 05:01:23 +0700

    Tugas nomor 1, luas kubus dan permukaan kubus

---
+++ Kubus22.java
@@ -0,0 +1,22 @@
+public class Kubus22 {
+            static double hitungVolume(double sisi) {
+                return sisi * sisi * sisi;
+            }
+        
+            static double hitungLuasPermukaan(double sisi) {
+                return 6 * sisi * sisi;
+            }
+        
+            public static void main(String[] args) {
+                double sisi = 5.0; 
+        
+                double volume = hitungVolume(sisi);
+                double luasPermukaan = hitungLuasPermukaan(sisi);
+        
+                System.out.println("Volume Kubus: " + volume);
+                System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+            }
+        }
+        
+    
+
