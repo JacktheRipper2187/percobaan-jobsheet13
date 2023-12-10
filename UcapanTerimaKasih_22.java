c4f5dbcc255b1e5c442783f3e6148e9bdeee044d
JackTheRipper2187 <ulilf442@gmail.com>
2023-12-10 20:11:18 +0700

    modifikasi percobaan 4, menambahkan fungsi ucapan tambahan

--- UcapanTerimaKasih_22.java
+++ UcapanTerimaKasih_22.java
@@ -11,8 +11,12 @@ public class UcapanTerimaKasih_22 {
     }
     public static void UcapanTerimakasih(){
         String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+" for being the best teacher in the world. \n"+
-        "You inspires in me a love for learning and made me feel like I could ask you anything. ");
+        System.out.println("Thank you "+nama+" for being the best teacher in the world.");
+        System.out.println(UcapanTambahan("You inspires in me a love for learning and made me feel like I could ask you anything."));
+    }
+    public static String UcapanTambahan(String pesan){
+        return pesan;
+
     }
     public static void main(String[] args) {
         UcapanTerimakasih();
